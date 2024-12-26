b107c2d5aff5ec298d14becc935b96b9096b66a1
Takeo Oyama <toyama@seagullscientific.com>
2024-12-26 11:12:36 +0900

    Add: GetBtwFileInformationAsync(Encoding encoding, CancellationToken cancellationToken = default) SliceAtCrlf(ReadOnlySpan<byte> readOnlySpan, Encoding encoding)

--- source/GetFileInformationFromBtwFile/GetFileInformationFromBtwFile.cs
+++ source/GetFileInformationFromBtwFile/GetFileInformationFromBtwFile.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 // ReSharper disable once IdentifierTypo
@@ -30,7 +31,7 @@ namespace ssapj.GetFileInformationFromBtwFile
             this._fileStream = File.OpenRead(filePath);
         }
 
-        public async ValueTask<bool> IsBtwFileAsync()
+        public async ValueTask<bool> IsBtwFileAsync(CancellationToken cancellationToken = default)
         {
             if (this._isBtwFile.alreadyCheckedHeader)
             {
@@ -39,14 +40,14 @@ namespace ssapj.GetFileInformationFromBtwFile
 
             this._bufferForHeader = new byte[26];
 
-            await this._fileStream.ReadAsync(this._bufferForHeader, 0, this._bufferForHeader.Length).ConfigureAwait(false);
+            await this._fileStream.ReadAsync(this._bufferForHeader, 0, this._bufferForHeader.Length, cancellationToken).ConfigureAwait(false);
 
             this._isBtwFile = (true, ((IStructuralEquatable)this._bufferForHeader).Equals(this._header, StructuralComparisons.StructuralEqualityComparer));
 
             return this._isBtwFile.isBtwFile;
         }
 
-        public async ValueTask<IEnumerable<string>> GetBtwFileInformationAsync()
+        public async ValueTask<IEnumerable<string>> GetBtwFileInformationAsync(CancellationToken cancellationToken = default)
         {
             if (!await this.IsBtwFileAsync())
             {
@@ -60,7 +61,7 @@ namespace ssapj.GetFileInformationFromBtwFile
             var crLfLength = 2;
             var copiedHeaderLength = this._header.Length - crLfLength;
 
-            await this._fileStream.ReadAsync(buffer, 0, buffer.Length);
+            await this._fileStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
 
             Buffer.BlockCopy(this._header, crLfLength, bufferForFileInformation, 0, copiedHeaderLength);
             Buffer.BlockCopy(buffer, 0, bufferForFileInformation, copiedHeaderLength, buffer.Length - copiedHeaderLength);
@@ -97,6 +98,58 @@ namespace ssapj.GetFileInformationFromBtwFile
             return SliceAtCrlf(bufferForFileInformation.AsSpan(0, length));
         }
 
+        public async ValueTask<IEnumerable<string>> GetBtwFileInformationAsync(Encoding encoding, CancellationToken cancellationToken = default)
+        {
+            if ((await this.IsBtwFileAsync()) == false)
+            {
+                throw new InvalidOperationException("This file is not a btw file.");
+            }
+
+            var length = 0;
+
+            var bufferForFileInformation = new byte[1023];
+            var buffer = new byte[999];
+            var crLfLength = 2;
+            var copiedHeaderLength = this._header.Length - crLfLength;
+
+            await this._fileStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
+
+            Buffer.BlockCopy(this._header, crLfLength, bufferForFileInformation, 0, copiedHeaderLength);
+            Buffer.BlockCopy(buffer, 0, bufferForFileInformation, copiedHeaderLength, buffer.Length - copiedHeaderLength);
+
+            for (var i = 0; i < bufferForFileInformation.Length - 7; i++)
+            {
+                if (bufferForFileInformation[i] == 0x89 &&
+                    bufferForFileInformation[i + 1] == 0x50 &&
+                    bufferForFileInformation[i + 2] == 0x4e &&
+                    bufferForFileInformation[i + 3] == 0x47 &&
+                    bufferForFileInformation[i + 4] == 0x0d &&
+                    bufferForFileInformation[i + 5] == 0x0a &&
+                    bufferForFileInformation[i + 6] == 0x1a &&
+                    bufferForFileInformation[i + 7] == 0x0a)
+                {
+                    for (var j = 1; j < i - 1; j++)
+                    {
+                        if (bufferForFileInformation[i - j] != 0x0A || bufferForFileInformation[i - j - 1] != 0x0d)
+                        {
+                            continue;
+                        }
+
+                        length = i - j - 1;
+                        break;
+                    }
+                }
+
+                if (length != 0)
+                {
+                    break;
+                }
+            }
+
+            return SliceAtCrlf(bufferForFileInformation.AsSpan(0, length), encoding);
+        }
+
+
         private static IEnumerable<string> SliceAtCrlf(ReadOnlySpan<byte> readOnlySpan)
         {
             var enc = Encoding.GetEncoding(65001);
@@ -119,6 +172,29 @@ namespace ssapj.GetFileInformationFromBtwFile
 
             return str;
         }
+
+        private static IEnumerable<string> SliceAtCrlf(ReadOnlySpan<byte> readOnlySpan, Encoding encoding)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var start = 0;
+            var stringList = new List<string>();
+
+            for (var i = 1; i < readOnlySpan.Length - 1; i++)
+            {
+                if (readOnlySpan[i] != 0x0d || readOnlySpan[i + 1] != 0x0a)
+                {
+                    continue;
+                }
+
+                stringList.Add(encoding.GetString(readOnlySpan.Slice(start, i - start).ToArray()));
+                start = i + 2;
+            }
+
+            stringList.Add(encoding.GetString(readOnlySpan.Slice(start).ToArray()));
+
+            return stringList;
+        }
+
 
         public void Dispose()
         {
